90abb1759329d87f18093621a87d8846dcee5e9f
Finlay Marno <finlay.marno@gmail.com>
2023-12-16 18:17:35 +0000

    update build to zig 11

--- build.zig
+++ build.zig
@@ -2,23 +2,26 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
-    opencl_rt(b, target, mode);
-    ze_rt(b, target, mode);
+    const optimize = b.standardOptimizeOption(.{});
+    opencl_rt(b, target, optimize);
+    ze_rt(b, target, optimize);
 }
 
-fn ze_rt(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode) void {
-    const exe = b.addExecutable("ze_rt", "src/ze_rt.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+fn ze_rt(b: *std.build.Builder, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) void {
+    const exe = b.addExecutable(.{
+        .name = "ze_rt",
+        .root_source_file = .{ .path = "src/ze_rt.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe.addLibraryPath("./deps/level-zero_1.10.0_win-sdk/lib");
-    exe.addIncludePath("./deps/level-zero_1.10.0_win-sdk/include");
+    //   exe.addLibraryPath("./deps/level-zero_1.10.0_win-sdk/lib");
+    //   exe.addIncludePath("./deps/level-zero_1.10.0_win-sdk/include");
     exe.linkSystemLibrary("ze_loader");
     exe.linkLibC();
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -28,19 +31,22 @@ fn ze_rt(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.M
     run_step.dependOn(&run_cmd.step);
 }
 
-fn opencl_rt(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode) void {
-    const exe = b.addExecutable("opencl_rt", "src/opencl_rt.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+fn opencl_rt(b: *std.build.Builder, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) void {
+    const exe = b.addExecutable(.{
+        .name = "opencl_rt",
+        .root_source_file = .{ .path = "src/opencl_rt.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe.addLibraryPath("C:/Program Files (x86)/IntelSWTools/system_studio_2020/OpenCL/sdk/lib/x64");
+    //exe.addLibraryPath("C:/Program Files (x86)/IntelSWTools/system_studio_2020/OpenCL/sdk/lib/x64");
     //exe.addLibraryPath("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/lib/x64");
-    exe.addIncludePath("./deps/opencl-headers");
+    exe.addIncludePath(.{ .path = "./deps/OpenCL-Headers" });
     exe.linkSystemLibrary("opencl");
     exe.linkLibC();
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
